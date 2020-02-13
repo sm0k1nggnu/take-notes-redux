a3e0053e4f4239495358ba920481d5501d3b3077
David May <me@davidmay.de>
2020-02-13 17:14:28 +0100

    not working

--- src/App.js
+++ src/App.js
@@ -1,68 +1,99 @@
 import React, {useState} from 'react';
+import { createStore } from 'redux';
 import NoteTable from './tables/NoteTable';
-import AddNoteForm from './forms/AddNoteForm'
-import EditNoteForm from './forms/EditNoteForm'
+import AddNoteForm from './forms/AddNoteForm';
+import EditNoteForm from './forms/EditNoteForm';
+import { Provider } from 'react-redux';
+import { connect } from 'react-redux';
+import {add_note, delete_note, edit_note } from './actions/actions';
+
+const Notes = [
+  {id: 1, title: 'A Note', content: 'Content of first note.'},
+  {id: 2, title: 'Another Note', content: 'Content of second note.'},
+  {id: 3, title: 'A Third Note', content: 'Content of third note.'},
+]
+
+const initialState = {
+  ...Notes
+};
+
+function reducer(state = initialState, action) {
+  console.log('reducer', state, action);
+  switch(action.type) {
+    case 'ADD_NOTE': {
+      return {
+        state
+      }
+    }
+    case 'DELETE_NOTE': {
+      return {
+        state
+      }
+    }
+    case 'EDIT_NOTE': {
+      return {
+        state
+      }
+    }
+    default:
+      return state;
+  }
+}
+
+const store = createStore(reducer);
+store.dispatch({ type: "ADD_NOTE" });
+store.dispatch({ type: "ADD_NOTE" });
+store.dispatch({ type: "DELETE_NOTE" });
+store.dispatch({ type: "EDIT_NOTE" });
 
 const App = () => {
-  const Notes = [
-    {id: 1, title: 'A Note', content: 'Content of first note.'},
-    {id: 2, title: 'Another Note', content: 'Content of second note.'},
-    {id: 3, title: 'A Third Note', content: 'Content of third note.'},
-  ]
   const initialFormState = { id: null, title: '', content: '' }
+  const editing = false;
 
-  const [notes, setNotes] = useState(Notes)
-  const [editing, setEditing] = useState(false)
-  const [currentNote, setCurrentNote] = useState(initialFormState)
+  // const [notes, setNotes] = useState(Notes)
+  // const [editing, setEditing] = useState(false)
+  // const [currentNote, setCurrentNote] = useState(initialFormState)
 
   const addNote = (note) => {
-    console.log(notes[notes.length-1].id)
-    note.id = notes[notes.length-1].id + 1//notes.length + 1 //doesn't work if you delete a note
-    setNotes([...notes, note])
+    // note.id = notes[notes.length-1].id + 1//take last entry, get id, add 1
+    // setNotes([...notes, note])
+    this.props.add_note();
   }
   const deleteNote = (id) => {
-    setNotes(notes.filter(note => note.id !== id))
+    //setNotes(notes.filter(note => note.id !== id))
+    this.props.delete_note();
   }
   const editRow = (note) => {
     //setNotes(notes.filter(note => note.id !== id))
-    setEditing(true)
-    setCurrentNote({id: note.id, title: note.title, content: note.content})
+    // setEditing(true)
+    // setCurrentNote({id: note.id, title: note.title, content: note.content})
+    this.props.edit_note();
   }
   const updateNote = (id, updatedNote) => {
-    setEditing(false)
-    setNotes(notes.map(
-      note => (note.id === id ? updatedNote : note)
-    ))
+    // setEditing(false)
+    // setNotes(notes.map(
+    //   note => (note.id === id ? updatedNote : note)
+    // ))
+    //this.props.dispatch({ type: "UPDATE_NOTE" });
   }
 
   return (
-    <div className="container">
-      <h1>Take some notes</h1>
-      <div className="flex-row">
-        <div className="flex-large">
-          <h2>{editing ? 'Edit' : 'Add'}  note</h2>
-          {editing ? (
-            <EditNoteForm
-            editing={editing}
-            setEditing={setEditing}
-            currentNote={currentNote}
-            updateNote={updateNote}
-          />
-          ) : (
-            <AddNoteForm addNote={addNote}/>
-          )}
-        </div>
-        <div className="flex-large">
-          <h2>View notes</h2>
-          <NoteTable
-            notes={notes}
-            deleteNote={deleteNote}
-            editRow={editRow}
-          />
-        </div>
-      </div>
-    </div>
+    <Provider store={store}>
+    <App />
+    </Provider>
   )
 }
 
-export default App;
+function mapStateToProps(state) {
+  return {
+    notes: state.Notes
+  };
+}
+
+const mapDispatchToProps = {
+  add_note,
+  delete_note,
+  edit_note
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
---
+++ src/actions/actions.js
@@ -0,0 +1,7 @@
+export const ADD_NOTE    = "ADD_NOTE";
+export const DELETE_NOTE = "DELETE_NOTE";
+export const EDIT_NOTE   = "EDIT_NOTE";
+
+export const add_note = () => ({ type: ADD_NOTE });
+export const delete_note = () => ({ type: DELETE_NOTE });
+export const edit_note = () => ({ type: EDIT_NOTE });
